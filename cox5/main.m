45fc8a9cd9776dca892af070ea19878f2b4bd921
Student <student@sdeh131-08c4al.jacks.local>
2015-04-02 12:07:37 -0500

    Initial Commit

---
+++ cox5/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cox5
+//
+//  Created by Student on 4/2/15.
+//  Copyright (c) 2015 sdsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
